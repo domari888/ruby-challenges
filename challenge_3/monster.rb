5b1c2b8a0279860033b37d41680d6d032bb67a04
damari <motokin128@gmail.com>
2021-05-24 09:34:31 +0900

    while の条件式とBrave　と Monster のattack の処理を修正

--- challenge_3/monster.rb
+++ challenge_3/monster.rb
@@ -1,11 +1,11 @@
 class Monster < Character
   def disp_monster
-    puts "#{name}が現れた！"
+    puts "#{@name}が現れた！"
   end
 
   def attack(brave)
-    puts "#{name}の攻撃！"
-    hitpoints = offence - brave.defence / 2
+    puts "#{@name}の攻撃！"
+    hitpoints = @offence - brave.defence / 2
     # hitpointがマイナスとなる場ダメージを 1 とする
     hitpoints = 1 if hitpoints <= 0 
     puts "#{brave.name}は#{hitpoints}のダメージを受けた！"
