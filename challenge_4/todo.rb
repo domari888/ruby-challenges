f60a243bcd6de0507917c2556b2ea9f882f133df
damari <motokin128@gmail.com>
2021-06-03 15:06:09 +0900

    add challenge_4

---
+++ challenge_4/todo.rb
@@ -0,0 +1,27 @@
+class ToDo
+  def initialize
+    @tasks = []
+  end
+
+  def add(task)
+    @tasks << task
+    puts "【追加】#{task.info}"
+  end
+
+  def index
+    puts "*=*=*=*=*=*=*=*=* task *=*=*=*=*=*=*=*=*"
+    @tasks.each do |task|
+      puts task.info
+    end
+    puts "*=*=*=*=*=*=*=*=*=*=*=**=*=*=*=*=*=*=*=*"
+  end
+
+  def delete(task_id)
+    if task = @tasks.find { |task| task.id == task_id[:id] }
+      puts "【削除】#{task.info}"
+      @tasks.delete(task)
+    else
+      puts "【！】該当idのタスクはありません。"
+    end
+  end
+end
